d1f12328d2611faa827444835dba5095c49cbb04
Artur <147540923+arturTheDev17@users.noreply.github.com>
2024-12-05 22:00:09 -0300

    aaa

--- src/main/java/net/weg/produtosestoque/controller/CategoriaController.java
+++ src/main/java/net/weg/produtosestoque/controller/CategoriaController.java
@@ -50,6 +50,7 @@ public class CategoriaController {
             categoria = service.atualizarCategoria(id, categoria);
             return new ResponseEntity<>( categoria , HttpStatus.OK);
         } catch ( Exception e ) {
+            System.out.println( e.getMessage() );
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- src/main/java/net/weg/produtosestoque/model/Categoria.java
+++ src/main/java/net/weg/produtosestoque/model/Categoria.java
@@ -1,8 +1,6 @@
 package net.weg.produtosestoque.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class Categoria {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false)
